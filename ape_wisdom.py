f1f5c871bc131040cea38a1e55128384aa23ff6b
cbratton85 <64664518+cbratton85@users.noreply.github.com>
2026-02-15 22:58:02 -0600

    Update ape_wisdom.py

--- ape_wisdom.py
+++ ape_wisdom.py
@@ -2192,8 +2192,9 @@ def export_interactive_html(df):
             "allow_symbol_change": false,
             "calendar": false,
             "studies": [
-                {"id": "MASimple@tv-basicstudies", "inputs": {"length": 50}, "overrides": { "Plot.color": "blue" }},
-                {"id": "MASimple@tv-basicstudies", "inputs": {"length": 200}, "overrides": { "Plot.color": "red" }}
+                {"id": "MASimple@tv-basicstudies", "inputs": {"length": 50}, "overrides": {"Plot.color": "blue"}},
+                {"id": "MASimple@tv-basicstudies", "inputs": {"length": 200}, "overrides": {"Plot.color": "red"}},
+                {"id": "VWMA@tv-basicstudies", "inputs": {"length": 20}, "overrides": {"Plot.color": "rgba(255, 255, 0, 0.5)", "Plot.linewidth": 4 }}
             ],
             "details": true,
             "hide_side_toolbar": true,
