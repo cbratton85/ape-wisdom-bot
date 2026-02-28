4f8da253485ec0f8395436f7cb872473e88c0481
cbratton85 <64664518+cbratton85@users.noreply.github.com>
2026-02-27 20:13:38 -0600

    Update ape_wisdom.py

--- ape_wisdom.py
+++ ape_wisdom.py
@@ -1485,11 +1485,13 @@ def export_interactive_html(df):
         <meta charset="UTF-8">
         <meta http-equiv="refresh" content="900">
         <title>Ape Wisdom Analysis</title>
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/x-icon" href="favicon.ico?v=1">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png?v=1">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png?v=1">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png?v=1">
         <link rel="manifest" href="site.webmanifest">
+        <link rel="icon" type="image/png" href="favicon.png?v=1">
+        <link rel="shortcut icon" href="favicon.ico?v=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
